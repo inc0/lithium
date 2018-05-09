2a23384e511a88db187ad5d402062787c1fba2eb
inc0 <inc007@gmail.com>
2018-05-09 12:48:29 -0700

    add mbr to dib

--- dib-centos/start.sh
+++ dib-centos/start.sh
@@ -1,3 +1,3 @@
 if [ ! -f /data/centos7.raw ]; then
-    disk-image-create centos7 baremetal dhcp-all-interfaces bootloader -o /data/centos7.raw -t raw -x
+    disk-image-create centos7 baremetal dhcp-all-interfaces block-device-mbr bootloader -o /data/centos7.raw -t raw -x
 fi
--- dib-ubuntu/start.sh
+++ dib-ubuntu/start.sh
@@ -1,3 +1,3 @@
 if [ ! -f /data/ubuntu1604.raw  ]; then
-    disk-image-create ubuntu baremetal dhcp-all-interfaces bootloader -o /data/ubuntu1604.raw -t raw -x
+    disk-image-create ubuntu baremetal dhcp-all-interfaces block-device-mbr bootloader -o /data/ubuntu1604.raw -t raw -x
 fi
