13b2b89a894c017e855b9b2df0bc669f5f83dd0b
inc0 <inc007@gmail.com>
2018-05-15 16:40:18 +0000

    add cloud-init

--- lithium-server/server.py
+++ lithium-server/server.py
@@ -15,17 +15,30 @@ args = parser.parse_args()
 
 @app.route('/kickstart/<mac_address>')
 def kickstart(mac_address):
+    print("Writing cloud config")
+    with open("/etc/cloud-init/default", "w") as f:
+        user_data = {
+            "meta-data": {},
+            "user-data":
+                {
+                    "users": [
+                        {
+                            "name": "lithium",
+                            "shell": "/bin/bash",
+                            "sudo": "ALL=(ALL) NOPASSWD: ALL",
+                            "ssh_authorized_keys": [args.pubkey,]
+                        }
+                    ]
+                }
+            }
+        f.write(json.dumps(user_data))
+    print("Serving template {}, {}, {}".format(args.image, args.pubkey, args.host_ip))
     return render_template('kickstart-simple.j2', image=args.image, pubkey=args.pubkey, host_ip=args.host_ip)
 
 
 @app.route('/v1/boot/<mac_address>')
 def index(mac_address):
     print(mac_address)
-    with open("./macs", "r") as f:
-        all_macs = f.read()
-    if mac_address in all_macs:
-        print("mac already provisioned")
-        return json.dumps({})
     with open("./macs", "a") as f:
         f.write(mac_address + " ")
     resp = {
@@ -33,7 +46,7 @@ def index(mac_address):
             'initrd': [
                 'http://{host_ip}:8010/pxeboot/initrd.img'.format(host_ip=args.host_ip),
             ],
-            'cmdline': 'text inst.repo=http://{host_ip}:8010/centos  ks=http://{host_ip}:8000/kickstart/{mac}'.format(
+            'cmdline': 'text inst.repo=http://{host_ip}:8010/centos  ks=http://{host_ip}:8000/kickstart/{mac} cloud-config-url=http://{host_ip}:8020'.format(
                 mac=mac_address,
                 host_ip=args.host_ip,
                 )
@@ -41,6 +54,4 @@ def index(mac_address):
     return json.dumps(resp)
 
 if __name__ == '__main__':
-    with open("./macs", "w") as f:
-        f.write("")
     app.run(debug=True, host="0.0.0.0", port=8000)
